bb00845ca95b485de196decacc1a9fc349555984
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-23 22:07:43 +0200

    Updating manifest && build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    kotlin("android") version "1.8.0"
 }
 
 android {
@@ -15,10 +14,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
@@ -30,10 +26,12 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17 // Actualiza a Java 17
+        targetCompatibility = JavaVersion.VERSION_17 // Actualiza a Java 17
     }
+
     buildFeatures {
         viewBinding = true
     }
@@ -45,17 +43,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     implementation("com.squareup.picasso:picasso:2.71828")
-
-    // Migrar a una versión compatible o alternativa si es necesario
-    implementation("androidx.exifinterface:exifinterface:1.3.3") // Reemplaza com.android.support:exifinterface
-
+    implementation("androidx.exifinterface:exifinterface:1.3.3")
+    implementation("com.github.blongho:worldCountryData:v1.5.3")
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-
-    // Reemplaza la biblioteca de datos del país con una versión compatible si es necesario
-    implementation("com.github.blongho:worldCountryData:v1.5.3") // Verifica compatibilidad con androidx
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
