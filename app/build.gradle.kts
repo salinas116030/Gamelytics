87543d61d3f882240dcf087d0c5feb465a5ea16d
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-23 21:10:51 +0200

    Fixing error Dependencies using groupId com.android.support and androidx.* can not be combined

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        vectorDrawables.useSupportLibrary = true
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -37,16 +39,22 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("com.github.blongho:worldCountryData:v1.5.3")
+
+    // Migrar a una versión compatible o alternativa si es necesario
+    implementation("androidx.exifinterface:exifinterface:1.3.3") // Reemplaza com.android.support:exifinterface
+
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    // Reemplaza la biblioteca de datos del país con una versión compatible si es necesario
+    implementation("com.github.blongho:worldCountryData:v1.5.3") // Verifica compatibilidad con androidx
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
