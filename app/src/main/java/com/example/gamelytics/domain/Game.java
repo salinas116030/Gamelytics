54907840abfdfd3c622058bb22a90eb8fbc85778
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-25 14:55:53 +0200

    Creating Game class in Domain

---
+++ app/src/main/java/com/example/gamelytics/domain/Game.java
@@ -0,0 +1,78 @@
+package com.example.gamelytics.domain;
+
+import androidx.annotation.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Game {
+
+    // Attributes
+    private String title;
+    private String logo;
+    private String cheapestPriceHistory;
+    private Map<String, Store> stores;
+    private int appID;
+
+    // Constructor
+    public Game() {
+        this.stores = new HashMap<String, Store>();
+    }
+
+    // Getters & Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getCheapestPriceHistory() {
+        return cheapestPriceHistory;
+    }
+
+    public void setCheapestPriceHistory(String cheapestPriceHistory) {
+        this.cheapestPriceHistory = cheapestPriceHistory;
+    }
+
+    public Map<String, Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(Map<String, Store> stores) {
+        this.stores = stores;
+    }
+
+    public int getAppID() {
+        return appID;
+    }
+
+    public void setAppID(int appID) {
+        this.appID = appID;
+    }
+
+    // Custom methods
+    public Store getStoreByDealId(String dealId) {
+        return stores.get(dealId);
+    }
+
+    public void addStore(String dealId, Store store) {
+        stores.put(dealId, store);
+    }
+
+    // System methods
+    @NonNull
+    @Override
+    public String toString() {
+        return this.title + "\n";
+    }
+}
