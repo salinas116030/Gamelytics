3718f86faccf51784d969a6e2da7e76f938cab7b
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-27 03:19:56 +0200

    Creating ApiGameRepository implementation to match Hexagonal Architecture

---
+++ app/src/main/java/com/example/gamelytics/infrastructure/ApiGameRepository.java
@@ -0,0 +1,44 @@
+package com.example.gamelytics.infrastructure;
+
+import com.example.gamelytics.domain.Game;
+import com.example.gamelytics.domain.GameRepository;
+import com.example.gamelytics.infrastructure.external.api.SharkApiClient;
+import com.example.gamelytics.infrastructure.external.api.services.Shark.GameApiService;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import java.util.List;
+
+public class ApiGameRepository implements GameRepository {
+    private final GameApiService gameApiService;
+
+    public ApiGameRepository() {
+        Retrofit retrofit = SharkApiClient.getClient();
+        gameApiService = retrofit.create(GameApiService.class);
+    }
+
+    @Override
+    public Game get(int id) {
+        Call<Game> call = gameApiService.getGame(id);
+        try {
+            Response<Game> response = call.execute();
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public List<Game> search(String name) {
+        Call<List<Game>> call = gameApiService.searchGames(name);
+        try {
+            Response<List<Game>> response = call.execute();
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
