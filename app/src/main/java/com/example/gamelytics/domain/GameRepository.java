0386a07f8fb95d61c3fdd7d76aafe390d11c7e6b
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-30 18:01:58 +0200

    Adding get parameter corrected

--- app/src/main/java/com/example/gamelytics/domain/GameRepository.java
+++ app/src/main/java/com/example/gamelytics/domain/GameRepository.java
@@ -1,8 +1,9 @@
 package com.example.gamelytics.domain;
 
+import com.example.gamelytics.domain.steam.GameSteam;
 import java.util.List;
 
 public interface GameRepository {
-    Game get(int id);
+    Game get(int id) ;
     List<GameItem> search(String name);
 }
