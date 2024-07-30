d6b250bf41ae9184aea1f70713ef8535d5e65de8
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-30 17:57:39 +0200

    Modifying Steam api client to suppport customize factory GameSteam

--- app/src/main/java/com/example/gamelytics/infrastructure/external/api/SteamApiClient.java
+++ app/src/main/java/com/example/gamelytics/infrastructure/external/api/SteamApiClient.java
@@ -1,5 +1,6 @@
 package com.example.gamelytics.infrastructure.external.api;
 
+import com.example.gamelytics.domain.steam.DynamicKeyConverterFactory;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,6 +12,7 @@ public class SteamApiClient {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .addConverterFactory(DynamicKeyConverterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
