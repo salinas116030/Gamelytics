fd805772acf575e91ea1c22a4ff0d68ac91c4580
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-07-30 17:58:44 +0200

    Modifying game api service from steam to support customize keyinterface

--- app/src/main/java/com/example/gamelytics/infrastructure/external/api/services/Steam/GameApiService.java
+++ app/src/main/java/com/example/gamelytics/infrastructure/external/api/services/Steam/GameApiService.java
@@ -1,11 +1,11 @@
 package com.example.gamelytics.infrastructure.external.api.services.Steam;
 
-import com.example.gamelytics.domain.GameItem;
+import com.example.gamelytics.domain.steam.GameSteam;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface GameApiService {
     @GET("appdetails")
-    Call<GameItem> getGame(@Query("appids") int id);
+    Call<GameSteam> getGame(@Query("appids") String id);
 }
