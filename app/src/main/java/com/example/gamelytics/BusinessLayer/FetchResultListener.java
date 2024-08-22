8a767bd41ebf26771d75c051a9cc90ddd7d20a26
Pol Salinas <elmendigo@MacBook-Air-de-Pol.local>
2024-08-22 15:42:59 +0200

    Refactoring deleting old-architecture files

--- app/src/main/java/com/example/gamelytics/BusinessLayer/FetchResultListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gamelytics.BusinessLayer;
-
-import com.example.gamelytics.DataLayer.Game;
-
-import org.json.JSONArray;
-
-public interface FetchResultListener {
-    void onResultFetched(Game result);
-}
